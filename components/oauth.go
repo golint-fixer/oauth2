0e22f18565858d97cb61e29324e290460d2c77f6
Poncholay <guillaume.wilmot@epitech.eu>
2015-08-19 10:34:06 +0200

    a

--- components/oauth.go
+++
@@ -1,8 +0,0 @@
-package components
-
-// AccessData represents an access request json response format.
-type AccessData struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	ExpiresIn    int    `json:"expires_in"`
-}
--- controllers/proxy.go
+++
@@ -1,6 +0,0 @@
-package controllers
-
-import "net/http"
-
-func Proxy(w http.ResponseWriter, r *http.Request) {
-}

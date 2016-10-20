7e39b3ae8f981e132d529ff1ee5b1d4815c6acfa
jb <jbaptiste.daniel@gmail.com>
2016-10-20 15:17:51 +0200

    ajout de la date de fin de campagne dans le model 'group'

--- models/group.go
+++ models/group.go
@@ -1,10 +1,16 @@
 // Definition of the structures and SQL interaction functions
 package models
 
+import (
+	"time"
+
+	// "github.com/asaskevich/govalidator"
+)
+
 // Group represents the componenents of a group
 type Group struct {
-	ID   uint    `json:"id"`
-	Name *string `json:"name,omitempty"`
-
-	Users []User `json:"contacts,omitempty"`
+	ID           uint       `json:"id"`
+	Name         *string    `json:"name,omitempty"`
+	Endofcampain *time.Time `json:"endofcampain"`
+	Users        []User     `json:"contacts,omitempty"`
 }
